84630eca512f421d03259dd41d9649323c4e9001
hrscott <henry.scott@ucsf.edu>
2023-01-24 22:06:58 -0800

    push

--- search/graph.py
+++ search/graph.py
@@ -1,28 +1,64 @@
+import sys
 import networkx as nx
+import pathlib
+import matplotlib
+import scipy
 
+#creating graph class
 class Graph:
-    """
-    Class to contain a graph and your bfs function
-    
-    You may add any functions you deem necessary to the class
-    """
     def __init__(self, filename: str):
-        """
-        Initialization of graph object 
-        """
-        self.graph = nx.read_adjlist(filename, create_using=nx.DiGraph, delimiter=";")
+        self.filename = filename
+        self.store = True
 
-    def bfs(self, start, end=None):
-        """
-        TODO: write a method that performs a breadth first traversal and pathfinding on graph G
-
-        * If there's no end node input, return a list nodes with the order of BFS traversal
-        * If there is an end node input and a path exists, return a list of nodes with the order of the shortest path
-        * If there is an end node input and a path does not exist, return None
-
-        """
+#creating instance of graph class
+        self.graph = nx.read_adjlist(filename, delimiter=";", create_using=nx.DiGraph)
+    
+#main bfs function: AKA "clunky boi" 
+def bfs(graph, source_node, end_node=None):
+        #creating empty list objects for queue visited nodes and traversal path
+    queue=[]
+    visited=[]
+    traversal=[]
+    if end_node == None:
+        visited.append(source_node)
+        queue.append(source_node)
+        #creating loop for visiting each node
+        while queue:
+            current_node= queue.pop(0)
+            traversal.append(current_node)
+            print(current_node, end=" ")
+        #checking all neighbor nodes of current node
+            for neighbor in graph[current_node]:
+                if neighbor not in visited :
+                    visited.append(neighbor)
+                    queue.append(neighbor)
+        return(graph)
+        ###implementing shortest path search if  end node is provided
+    if end_node != None:
+        #initializing new list of visited nodes for shortest path implementation
+        visitedSP = []
+        queue = [[source_node]]
+        #checking possible edge case where source_node= end_node
+        if source_node == end_node:
+            print("Same Node")
+            return
+        # Loop to traverse the graph with the help of the queue
+        while queue:
+            path = queue.pop(0)
+            node = path[-1]
+        # Condition to check whether or not node is in visitedSP
+            if node not in visitedSP:
+                neighbours = graph[node]
+        # Loop to iterate over the neighbours of the node
+                for neighbour in neighbours:
+                    new_path = list(path)
+                    new_path.append(neighbour)
+                    queue.append(new_path)
+        # Condition to check if the neighbour node is the goal
+                    if neighbour == end_node:
+                        print(new_path)
+                        return new_path
+                visitedSP.append(node)
+        #conditon to cehck if no path to the end_node exists
+        print("connecting path does not exist")
         return
-
-
-
-
--- test/test_bfs.py
+++ test/test_bfs.py
@@ -1,27 +1,47 @@
 # write tests for bfs
 import pytest
-from search import graph
+import search
+import pathlib
+import difflib
+import networkx as nx
 
-@pytest.fixture
-def test_bfs_traversal():
-    """
-    TODO: Write your unit test for a breadth-first
-    traversal here. Create an instance of your Graph class 
-    using the 'tiny_network.adjlist' file and assert 
-    that all nodes are being traversed (ie. returns 
-    the right number of nodes, in the right order, etc.)
-    """
-    pass
+#creating objects that represent paths to data
+citation_network_dir= pathlib.Path.cwd().resolve().parent / "data/citation_network.adjlist"
+tiny_network_dir= pathlib.Path.cwd().resolve().parent / "data/tiny_network.adjlist"
 
+#creating graph objects from above paths
+data_full= nx.read_adjlist(citation_network_dir, delimiter=";", create_using=nx.DiGraph)
+data_tiny= nx.read_adjlist(tiny_network_dir, delimiter=";", create_using=nx.DiGraph)
+
+
+### TESTING
+
+#testing to ensure that my bfs function yields a full traversal of the citation_network dataset 
+
+#printing the dimensions of the citation_network dataset (yields 5210 nodes and 9247 edges)
+print(data_full)
+
+def test_full_traversal():
+  
+   data_full= nx.read_adjlist(citation_network_dir, delimiter=";", create_using=nx.DiGraph)
+   my_data=list(search.bfs(data_full, source_node="Lani Wu"))
+
+   assert len(my_data)==5210
+
+
+#testing to ensure that my shortest-path functionality is valid via comparison with the output of networkx's
+# bidirectional_shortest_path function
 def test_bfs():
-    """
-    TODO: Write your unit test for your breadth-first 
-    search here. You should generate an instance of a Graph
-    class using the 'citation_network.adjlist' file 
-    and assert that nodes that are connected return 
-    a (shortest) path between them.
-    
-    Include an additional test for nodes that are not connected 
-    which should return None. 
-    """
+#creating example of networkx bidirectional_shortest_path
+    test_data1=nx.bidirectional_shortest_path(data_full, source="Lani Wu", target="Marina Sirota")
+# recreating the same shortest path given identical start/end nodes with my function
+    my_data1=search.bfs(data_full,"Lani Wu","Marina Sirota")
+
+    assert my_data1==test_data1
+
+
+#providing traversal of mini data set as requested ingrading rubric??
+#this test is just intended to show the output traversal
+def test_traversal_mini_data():
+    my_data2=search.bfs(data_tiny,"Lani Wu")
     pass
